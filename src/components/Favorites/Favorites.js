a103776a817adadb5c1c7490dbe7f4423509c68b
Michael KS <kingstockton@gmail.com>
2019-06-06 23:33:44 -0600

    add media queries

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import './Favorites.scss'
+//Coming 4 quarter 2019
 
-const Favorites = () => {
+// import React from 'react'
+// import './Favorites.scss'
 
-  return (
-    <div className="Favorites-btn">
-      <div role="button" >View Favorites</div>
-      <p className="Favorites-click">0</p>
-    </div>
-  )
-}
+// const Favorites = () => {
 
-export default Favorites
+//   return (
+//     <div className="Favorites-btn">
+//       <div role="button" >View Favorites</div>
+//     </div>
+//   )
+// }
+
+// export default Favorites
