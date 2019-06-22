c9708b582755365e51cc5aa32beed9ec5f4e3e02
Michael KS <kingstockton@gmail.com>
2019-06-22 12:56:59 -0600

    Updates

---
+++ precache-manifest.16d454229fd33cfe79825f01ec3009bb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4288377a0ab7cc6bd79d63fd1999b0cb",
+    "url": "/swapi-box/index.html"
+  },
+  {
+    "revision": "eb03a88264631950d134",
+    "url": "/swapi-box/static/css/main.bf7f810f.chunk.css"
+  },
+  {
+    "revision": "8f4d0ec67759b3eb51de",
+    "url": "/swapi-box/static/js/2.917bba69.chunk.js"
+  },
+  {
+    "revision": "eb03a88264631950d134",
+    "url": "/swapi-box/static/js/main.976d5a5a.chunk.js"
+  },
+  {
+    "revision": "131d28aeaaf818238108",
+    "url": "/swapi-box/static/js/runtime~main.a3482073.js"
+  },
+  {
+    "revision": "e7f35859217514e01da14ac0ba69fbbb",
+    "url": "/swapi-box/static/media/DeathStar.e7f35859.svg"
+  },
+  {
+    "revision": "fbdbc00becc0796ad473f10a9ba016f0",
+    "url": "/swapi-box/static/media/Falcon.fbdbc00b.svg"
+  },
+  {
+    "revision": "6f56c0f13e733da4e46cad6a2233f837",
+    "url": "/swapi-box/static/media/StarWarsLogo.6f56c0f1.jpg"
+  },
+  {
+    "revision": "0043575e1b1edebe347edbef2d50b74c",
+    "url": "/swapi-box/static/media/Stroop.0043575e.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/swapi-box/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d072b762de234e54b759b3e216a0fb9e",
+    "url": "/swapi-box/static/media/noFav.d072b762.svg"
+  }
+]);
